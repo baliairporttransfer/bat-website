73158e16dc062ce52aa06e4da22e5a404fb02d34
baliairporttransfer <hello.baliairporttransfer@gmail.com>
2026-08-01 22:58:48 +0800

    Update prices.ts

--- data/prices.ts
+++ data/prices.ts
@@ -1,74 +1,83 @@
 export type DestinationPrice = {
-      id: string;
-        name: string;
-          price: number;
-          };
+  id: string;
+  name: string;
+  price: number;
+};
 
-
-          export const destinationPrices: DestinationPrice[] = [
-            {
-                id: "kuta-seminyak-kerobokan",
-                    name: "Kuta / Seminyak / Kerobokan",
-                        price: 225000,
-                          },
-                            {
-                                id: "nusa-dua-jimbaran",
-                                    name: "Nusa Dua / Jimbaran",
-                                        price: 240000,
-                                          },
-                                            {
-                                                id: "sanur-denpasar",
-                                                    name: "Sanur / Denpasar",
-                                                        price: 250000,
-                                                          },
-                                                            {
-                                                                id: "ubud-area",
-                                                                    name: "Ubud Area",
-                                                                        price: 350000,
-                                                                          },
-                                                                            {
-                                                                                id: "payangan-tegallalang-pejeng",
-                                                                                    name: "Payangan / Tegallalang / Pejeng",
-                                                                                        price: 375000,
-                                                                                          },
-                                                                                            {
-                                                                                                id: "bedugul",
-                                                                                                    name: "Bedugul",
-                                                                                                        price: 550000,
-                                                                                                          },
-                                                                                                            {
-                                                                                                                id: "munduk",
-                                                                                                                    name: "Munduk",
-                                                                                                                        price: 650000,
-                                                                                                                          },
-                                                                                                                            {
-                                                                                                                                id: "lovina",
-                                                                                                                                    name: "Lovina",
-                                                                                                                                        price: 750000,
-                                                                                                                                          },
-                                                                                                                                            {
-                                                                                                                                                id: "sumberkima-pemuteran-menjangan",
-                                                                                                                                                    name: "Sumberkima / Pemuteran / Menjangan",
-                                                                                                                                                        price: 850000,
-                                                                                                                                                          },
-                                                                                                                                                            {
-                                                                                                                                                                id: "uluwatu",
-                                                                                                                                                                    name: "Uluwatu",
-                                                                                                                                                                        price: 350000,
-                                                                                                                                                                          },
-                                                                                                                                                                            {
-                                                                                                                                                                                id: "canggu-pererenan-mengwi",
-                                                                                                                                                                                    name: "Canggu / Pererenan / Mengwi",
-                                                                                                                                                                                        price: 325000,
-                                                                                                                                                                                          },
-                                                                                                                                                                                            {
-                                                                                                                                                                                                id: "tanah-lot-tabanan",
-                                                                                                                                                                                                    name: "Tanah Lot / Tabanan",
-                                                                                                                                                                                                        price: 370000,
-                                                                                                                                                                                                          },
-                                                                                                                                                                                                            {
-                                                                                                                                                                                                                id: "amed",
-                                                                                                                                                                                                                    name: "Amed",
-                                                                                                                                                                                                                        price: 750000,
-                                                                                                                                                                                                                          },
-                                                                                                                                                                                                                          ];
+export const destinationPrices: DestinationPrice[] = [
+  {
+    id: "kuta-seminyak-kerobokan",
+    name: "Kuta / Seminyak / Kerobokan",
+    price: 225000,
+  },
+  {
+    id: "nusa-dua-jimbaran",
+    name: "Nusa Dua / Jimbaran",
+    price: 240000,
+  },
+  {
+    id: "sanur-denpasar",
+    name: "Sanur / Denpasar",
+    price: 250000,
+  },
+  {
+    id: "canggu-pererenan-mengwi",
+    name: "Canggu / Pererenan / Mengwi",
+    price: 325000,
+  },
+  {
+    id: "ubud-area",
+    name: "Ubud Area",
+    price: 350000,
+  },
+  {
+    id: "uluwatu",
+    name: "Uluwatu",
+    price: 350000,
+  },
+  {
+    id: "tanah-lot-tabanan",
+    name: "Tanah Lot / Tabanan",
+    price: 370000,
+  },
+  {
+    id: "payangan-tegallalang-pejeng",
+    name: "Payangan / Tegallalang / Pejeng",
+    price: 375000,
+  },
+  {
+    id: "padangbai",
+    name: "Padangbai",
+    price: 500000,
+  },
+  {
+    id: "bedugul",
+    name: "Bedugul",
+    price: 550000,
+  },
+  {
+    id: "sidemen",
+    name: "Sidemen",
+    price: 600000,
+  },
+  {
+    id: "munduk",
+    name: "Munduk",
+    price: 650000,
+  },
+  {
+    id: "lovina",
+    name: "Lovina",
+    price: 750000,
+  },
+  {
+    id: "amed-lempuyang",
+    name: "Amed / Lempuyang",
+    price: 750000,
+  },
+  {
+    id: "sumberkima-pemuteran-menjangan",
+    name: "Sumberkima / Pemuteran / Menjangan",
+    price: 850000,
+  },
+];
